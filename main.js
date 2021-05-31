d13572dd916857a6f3e40d01a402d61e0d61968b
vinegarboy <necogusa398@gmail.com>
2021-05-31 11:42:15 +0900

    変換器作成

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-function
+function AReplace(before,x){
+	var result=before;
+	while(before.indexOf(x)!=-1){
+		result = result.replace(x,'');
+	}
+	return result;
+}
 
 var m_honbun = document.getElementById("novel_honbun").innerHTML;
-var honbun_1 = m_honbun.replace("</p>","");
+var honbun_1 = AReplace(AReplace(m_honbun,'</p>'),'<br>');
+
 var i = 1;
 while(honbun_1.indexOf('<p id="L'+i+'">')!=-1){
 	honbun_1=honbun_1.replace('<p id="L'+i+'">','');
