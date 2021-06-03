8ca7457b8f3992ae7347df336d175828f873dc80
vinegarboy <necogusa398@gmail.com>
2021-06-03 11:45:05 +0900

    少し修正

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-function AReplace(before,x){
-	var result=before;
-	while(before.indexOf(x)!=-1){
-		result = result.replace(x,'');
-	}
-	return result;
+function AReplace(before, x) {
+    var result = before;
+    while (before.indexOf(x) != -1) {
+        result = result.replace(x, '');
+    }
+    return result;
 }
 
 var m_honbun = document.getElementById("novel_honbun").innerHTML;
-var honbun_1 = AReplace(AReplace(m_honbun,'</p>'),'<br>');
+var honbun_1 = AReplace(AReplace(m_honbun, '</p>'), '<br>');
 
 var i = 1;
-while(honbun_1.indexOf('<p id="L'+i+'">')!=-1){
-	honbun_1=honbun_1.replace('<p id="L'+i+'">','');
-	i++;
+while (honbun_1.indexOf('<p id="L' + i + '">') != -1) {
+    honbun_1 = honbun_1.replace('<p id="L' + i + '">', '');
+    i++;
 }
+console.log(honbun_1);
